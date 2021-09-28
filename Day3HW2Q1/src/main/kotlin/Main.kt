38c23e6c0dfc0e6259d0f08ac13ad18ebd10b96f
Saad alhajri <saadalhajri@Saads-MacBook-Air.local>
2021-09-29 02:01:54 +0300

    homework2

---
+++ Day3HW2Q1/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println(add(10, 12))
+
+}
+
+fun add(a: Int, b: Int): Int {
+    var r = a + b
+    return (r)
+}
---
+++ Day3HW2Q2/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+
+    full_name(firstname = "Ahmed", middlename = "Abdullah", lastname = "AlWaleed")
+}
+
+fun full_name(firstname: String, middlename: String, lastname: String) {
+    println("$firstname $middlename $lastname")
+}
