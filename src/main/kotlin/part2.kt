e376377cf322f3e4bac2e8ea80614213f6d9cb59
HalaAlbaqawi <hala.hammed.dmm.k1@tuwaiq.edu.sa>
2021-09-29 17:23:24 +0300

    Homewoork2

---
+++ src/main/kotlin/part2.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var num1 = 10
+    var num2 = 12
+    add(10, 12)
+    fullname("ahmad","abdullah","alwaleed")
+
+}
+fun fullname(first: String, middle: String, last: String) {
+    println("the name is $first $middle $last")
+}
+fun add(num1: Int, num2: Int) {
+    println(num1 + num2)
+}
