8f1391b89ca7f439ebccafd0bd596e6f9075c644
Arwa Saleh Alomari <91452271+ArwaSAO@users.noreply.github.com>
2021-09-29 06:43:27 +0300

    Add files via upload

---
+++ solution part1.kt
@@ -0,0 +1,23 @@
+//part1 Q 1
+
+
+/*fun main() {
+
+   val add( n1=10 , n2=12 )
+
+}
+
+fun add ( n1:Int , n2:Int):Int {
+   var result = n1 + n2
+    println(result )
+}*/
+
+// part 1 Q2
+
+/*  fun main(){
+  val  fullName (name 1= Ahmed , name2= Abdullah, name3= Al Waleed )
+  }
+
+fun fullName (name1,name2, name3) {
+println(fullName("Ahmed","Abdullah","Al Waleed"))
+}*/
---
+++ solution part2.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val maximum(num1=10, num=12, num=18)
+}
+
+val maximum = fun(num1:Int, num2:Int, num3:Int){
+    if (num1>=num2){
+        if(num1>=num3){
+            println( "the maximum number is $num1")
+        }
+        else if (num2>=num3) {
+            println("the maximum number is $num2")
+        }
+        else {
+            println("the maximum number is $num3")
+        }
+    }
