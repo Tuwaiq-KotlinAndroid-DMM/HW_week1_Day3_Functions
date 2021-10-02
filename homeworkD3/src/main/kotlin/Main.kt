8f00a5ad383e54c982843419bc7bbb1e5b790b3b
Nourah Mohammed <n.alnasser.2014@gmail.com>
2021-10-03 01:53:22 +0300

    Nourah HWD3

---
+++ homeworkD3/src/main/kotlin/Main.kt
@@ -0,0 +1,127 @@
+//  Part 1
+//fun main(args: Array<String>) {
+//
+//    println("Enter first number: ")
+//    var x : Int = readLine()!!.toInt()
+//
+//    println("Enter scound number: ")
+//    var y : Int = readLine()!!.toInt()
+//
+//
+//    var z = add(x,y)
+//
+//    println("The sum is: $z")
+//
+//    add1(3,8)
+//
+//
+//}
+//
+//fun add1 (x :Int , y: Int){
+//    print( x+y)
+//}
+//
+//fun add (x : Int , y: Int):Int {
+//
+//    return x + y
+//}
+//
+
+
+//
+//
+//fun main(args: Array<String>) {
+//
+//
+//    println("Enter your first name: ")
+//    var firsName : String = readLine()!!.toString()
+//
+//    println("Enter your middle name: ")
+//    var middleName : String = readLine()!!.toString()
+//
+//    println("Enter your last name: ")
+//    var lastName :String = readLine()!!.toString()
+//
+//
+//    fullName(firsName ,middleName,lastName)
+//
+//}
+//
+//fun fullName (firstName: String, middleName: String, lastName: String){
+//    var fullName : String = "$firstName $middleName $lastName"
+//    println("Your full name : $fullName")
+//}
+
+
+
+// part 2
+//
+//fun main(args: Array<String>) {
+//
+//    println("Enter Frist number: ")
+//    var x : Int = readLine()!!.toInt()
+//
+//
+//    println("Enter Second number: ")
+//    var y : Int = readLine()!!.toInt()
+//
+//
+//    println("Enter Third number: ")
+//    var z : Int = readLine()!!.toInt()
+//
+//    maximum(x,y,z)
+//}
+//
+//
+//val maximum : (Int, Int, Int) -> Unit ={
+//        x,y,z ->
+//    if (x > y && x > z ){
+//        println (x)
+//    }
+//    else if ( y > x && y > z)
+//    {
+//        println( y )
+//    }
+//    else
+//    {
+//        println(z)
+//    }
+//}
+
+
+
+// part 3
+
+//
+//fun main(args: Array<String>) {
+//
+//    println("Enter the first number: ")
+//    var x = readLine()!!.toInt()
+//
+//    println("Enter the secound number: ")
+//    var y = readLine()!!.toInt()
+//
+//
+//    var z: String = math(x,y)
+//
+//
+//    println(z)
+//}
+//
+//fun math (x: Int, y:Int):String{
+//    var sum : Int = x + y
+//    var multiple : Int = x * y
+//    var subtract : Int = x - y
+//    var divide =
+//        if (y != 0)
+//            x / y
+//        else
+//            "connot divide by 0"
+//
+//
+//    return "The Sum is: $sum \n" +
+//            "The multiply is: $multiple \n" +
+//            "The subtract is: $subtract \n" +
+//            "The divide is: $divide \n"
+//}
+
